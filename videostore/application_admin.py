e0605cc092785f9a3d64985a7668b03679421ef5
Axel Díaz <diaz.axelio@gmail.com>
2014-11-02 13:39:19 +0000

    Inicializando proyecto

---
+++ videostore/application_admin.py
@@ -0,0 +1,27 @@
+
+from camelot.view.art import Icon
+from camelot.admin.application_admin import ApplicationAdmin
+from camelot.admin.section import Section
+from camelot.core.utils import ugettext_lazy as _
+
+class MyApplicationAdmin(ApplicationAdmin):
+  
+    name = 'VideoStore'
+    application_url = 'http://www.python-camelot.com'
+    help_url = 'http://www.python-camelot.com/docs.html'
+    author = 'Axelio'
+    domain = 'mydomain.com'
+    
+    def get_sections(self):
+        from camelot.model.memento import Memento
+        from camelot.model.i18n import Translation
+        return [ Section( _('My classes'),
+                          self,
+                          Icon('tango/22x22/apps/system-users.png'),
+                          items = [] ),
+                 Section( _('Configuration'),
+                          self,
+                          Icon('tango/22x22/categories/preferences-system.png'),
+                          items = [Memento, Translation] )
+                ]
+    
